42c8a7118a9dabaca6cb66706dedd5544c8be16a
daniel <yoojeenkim@gmail.com>
2021-11-14 16:19:17 -0500

    added nav component

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navigation({ currentPage, handlePageChange }) {
+    return (
+        <ul className="nav">
+            <li className="nav-item">
+                <a href="#home" onClick={() => handlePageChange('Home')} className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Home</a>
+            </li>
+            <li className="nav-item">
+                <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a>
+            </li>
+            <li className="nav-item">
+                <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
+            </li>
+            <li className="nav-item">
+                <a href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
+            </li>
+        </ul>
+    )
+}
+export default Navigation;
