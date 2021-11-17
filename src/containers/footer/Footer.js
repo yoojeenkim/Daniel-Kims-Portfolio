7fd31bba5b5ec6ce3adc5df416616d0d723fbe57
daniel <yoojeenkim@gmail.com>
2021-11-17 00:04:51 -0500

    added icons

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaReddit, FaTwitter, FaStackOverflow } from 'react-icons/fa';
+import '../footer/Footer.css';
 
 function Footer () {
     return (
         <div className="container">
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div className="col-md-4 d-flex align-items-center"></div>
-                <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-                    <li className="ms-3">
-                        <span>LinkedIn</span>
+            <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
+                <div className="d-flex align-items-center"></div>
+                <ul className="nav justify-content-center d-flex">
+                    <li className="ms-3 icon">
+                        <FaLinkedin />
                     </li>
-                    <li className="ms-3">
-                        <span>GitHub</span>
+                    <li className="ms-3 icon">
+                        <FaGithub />
                     </li>
-                    <li className="ms-3">
-                        <span>Reddit</span>
+                    <li className="ms-3 icon">
+                        <FaReddit />
                     </li>
-                    <li className="ms-3">
-                        <span>Twitter</span>
+                    <li className="ms-3 icon">
+                        <FaTwitter />
                     </li>
-                    <li className="ms-3">
-                        <span>Stack Overflow</span>
+                    <li className="ms-3 icon">
+                        <FaStackOverflow />
                     </li>
                 </ul>
             </footer>
