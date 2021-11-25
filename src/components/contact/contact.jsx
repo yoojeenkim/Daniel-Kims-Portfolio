542ffc38f4b76811a382c4a20d1be35a9e3ac327
daniel <yoojeenkim@gmail.com>
2021-11-25 02:09:52 -0500

    added more stuff to about me and contact sections

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,8 +2,15 @@ import "./contact.scss"
 
 export default function Contact() {
     return (
-        <div className="contact">
-            
+        <div className="contact" id="contact">
+            <div className="right">
+                <h2>Contact Me!</h2>
+                <form>
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -3,7 +3,15 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#about"> </a>
+                </div>
+                <h1>Daniel Kim's Portfolio</h1>
+                <div className="right">
+
+                </div>
+            </div>
         </div>
     )
 }
